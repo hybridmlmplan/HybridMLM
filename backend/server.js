e3f0b5c12f4323861e7b16497b766b39e2ffc9c7
hybridmlmplan <osfdoi@gmail.com>
2025-11-25 20:59:24 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Backend Server Running Successfully!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
